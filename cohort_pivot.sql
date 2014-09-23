27a27bd0575232826c4d1fd6df51dfd42af2d5ea
Alex F. Bokov <alex.bokov@gmail.com>
2014-09-23 15:45:55 -0500

    Finished static pivot and added detailed comments explaining what is constant and what might change

--- cohort_pivot.sql
+++ cohort_pivot.sql
@@ -1,38 +1,45 @@
 -- create table of patients and how many distinct START_DATEs each
 -- had during each year when they had visits
--- NOT SPECIFIC TO ANY GIVEN COHORT OR SELECTION CRITERIA
-
+-- SAME FOR ALL STUDIES
 drop table patient_yr_visits;
 create table patient_yr_visits as
 select patient_num, start_date, concept_cd, extract(year from start_date) yr
+-- not all sites use BLUEHERONDATA, should be a substitution variable
+-- but all sites should have an OBSERVATION_FACT there regardless of what the
+-- schema is named
 from blueherondata.observation_fact
 group by patient_num, start_date, concept_cd;
 
+-- create a table of unique concept descriptors (for filtering later on)
+-- SAME FOR ALL STUDIES
 drop table unique_conc;
 create table unique_conc as
 select concept_cd,min(concept_path) concept_path,min(name_char) name_char
+-- not all sites use BLUEHERONDATA, should be a substitution variable
+-- but all sites should have an CONCEPT_DIMENSION there regardless of what the
+-- schema is named
 from blueherondata.concept_dimension
 group by concept_cd;
 
+-- merge the two above
+-- SAME FOR ALL STUDIES
 drop table patient_yr_conc;
 create table patient_yr_conc as
 select py.*, concept_path, name_char
 from patient_yr_visits py 
 join unique_conc uc
 on py.concept_cd = uc.concept_cd
-left join blueherondata.patient_dimension pd
-on py.patient_num = pd.patient_num
 ;
 
 -- this is the table on which pivots will be run
 drop table patient_counts_yr0;
 create table patient_counts_yr0 as
 with
--- ethnicity
+-- ethnicity (might as well use always, in case needed)
 eth as (select distinct patient_num,concept_cd ethno
 from blueherondata.observation_fact
 where concept_cd like 'DEM|ETHNICITY:%'),
--- age group
+-- age group (might as well use always, in case needed)
 ages as (select distinct patient_num,
 case 
  when age_in_years_num is NULL then 'Unknown'
@@ -44,10 +51,13 @@ case
  when age_in_years_num <= 85 then '65-85 years'
  else '85+ years'
 end age_group from blueherondata.patient_dimension),
--- total # visits that year
+-- total # visits that year (needed for all studies)
 tot as (select patient_num,yr,count(*) visits 
   from (select distinct patient_num,yr,start_date from patient_yr_conc)
   group by patient_num,yr),
+/*** BEGIN STUDY-SPECIFIC ALIASED SUBQUERIES ***/
+-- there may be some way to read the names an corresponding criteria 
+-- from a table and dynamically write the rather repetitious SQL below
 -- total # BMI measurements or obesity diagnoses for each patient each year
 bmiob as (select patient_num,yr,count(*) bmi_or_ob 
   from (select distinct patient_num,yr,start_date from patient_yr_conc
@@ -87,7 +97,9 @@ amph as(
   where (concept_cd like 'RXCUI:%' or concept_cd like '%MEDICATION_ID:%')
   and concept_path like '%\3257561\3257589%')
   group by patient_num,yr)
-
+/*** END STUDY-SPECIFIC ALIASED SUBQUERIES ***/
+-- what should be selected can be inferred from what aliased subqueries were
+-- created in addition to what columns exist in PATIENT_DIMENSION
 select pd.patient_num, sex_cd,race_cd,vital_status_cd,marital_status_cd,
 ethno, age_group, tot.yr, visits, bmi_or_ob, exclusions_diag, cholesterol, lipid_panels
 from tot 
@@ -113,12 +125,24 @@ on tot.patient_num = pd.patient_num
 
 -- The Pivot:
 -- total number of visits each year by patients grouped by the below criteria
-select *
-from patient_counts_yr0
+select * from (select 
+  -- the user might wish to omit some of these or add new custom ones
+  -- all of these will be categoric variables
+  sex_cd,race_cd,vital_status_cd
+  ,marital_status_cd,ethno,age_group
+  -- yr is always going to be here and will be an integer
+  ,yr
+  -- this can vary; for the tables as defined here it can be one of:
+  -- visits bmi_or_ob, exclusions_diag, cholesterol, lipid_panels
+  ,bmi_or_ob
+  from patient_counts_yr0)
 pivot(
-  -- bmi_or_ob, exclusions_diag, cholesterol, lipid_panels
-  count(bmi_or_ob) visits
+  -- can be either...
+  -- count(visits) patients
+  -- ...or...
+  sum(bmi_or_ob) visits
 FOR yr IN (
+  -- might be nice to have dynamically settable (or even guessable) years
   1999, 2000, 2001, 2002, 2003, 2004, 
   2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014)
 );
