c95eeab761963a9e4491e865d265102a7b3e2d7d
Alex F. Bokov <alex.bokov@gmail.com>
2014-09-23 16:01:45 -0500

    Added 'order by' to pivot

--- cohort_pivot.sql
+++ cohort_pivot.sql
@@ -145,4 +145,7 @@ FOR yr IN (
   -- might be nice to have dynamically settable (or even guessable) years
   1999, 2000, 2001, 2002, 2003, 2004, 
   2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014)
-);
+)
+-- this can vary
+order by vital_status_cd,ethno,race_cd,sex_cd,marital_status_cd,age_group
+;
