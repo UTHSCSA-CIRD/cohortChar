045751d8ee1600c16e4367b7f5c8c47d23e05314
Alex F. Bokov <alex.bokov@gmail.com>
2014-09-24 11:02:17 -0500

    Stripped out some junk comments and added in the CONCEPTS_OBESITY table of exclusion criteria

--- cohort_pivot.sql
+++ cohort_pivot.sql
@@ -38,9 +38,86 @@ define pivotvars = "sex_cd,race_cd,vital_status_cd,marital_status_cd,ethno,age_g
 define pivotorder = "vital_status_cd,ethno,race_cd,sex_cd,marital_status_cd,age_group";
 define vnames = "&&name0 , &&name1 , &&name2 , &&name3 , &&name4 , &&name5 ";
 
+/*** exclusion diagnoses specific to obesity cohort ***/
+drop table concepts_obesity;
+create table concepts_obesity as
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:042%' or (concept_cd like '%DX_ID:%' and concept_path like '%\042.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:242.9%' or (concept_cd like '%DX_ID:%' and concept_path like '%\242.9_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:243%' or (concept_cd like '%DX_ID:%' and concept_path like '%\243.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:262%' or (concept_cd like '%DX_ID:%' and concept_path like '%\262.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:263.2%' or (concept_cd like '%DX_ID:%' and concept_path like '%\263.2_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:263.8%' or (concept_cd like '%DX_ID:%' and concept_path like '%\263.8_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:275.49%' or (concept_cd like '%DX_ID:%' and concept_path like '%\275.49\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:281.4%' or (concept_cd like '%DX_ID:%' and concept_path like '%\281.4_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:307.1%' or (concept_cd like '%DX_ID:%' and concept_path like '%\307.1_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:530.13%' or (concept_cd like '%DX_ID:%' and concept_path like '%\530.13\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:755.24%' or (concept_cd like '%DX_ID:%' and concept_path like '%\755.24\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:755.34%' or (concept_cd like '%DX_ID:%' and concept_path like '%\755.34\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:755.69%' or (concept_cd like '%DX_ID:%' and concept_path like '%\755.69\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:756%' or (concept_cd like '%DX_ID:%' and concept_path like '%\756.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:756.4%' or (concept_cd like '%DX_ID:%' and concept_path like '%\756.4_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:756.9%' or (concept_cd like '%DX_ID:%' and concept_path like '%\756.9_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:757.2%' or (concept_cd like '%DX_ID:%' and concept_path like '%\757.2_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:759.82%' or (concept_cd like '%DX_ID:%' and concept_path like '%\759.82\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:759.89%' or (concept_cd like '%DX_ID:%' and concept_path like '%\759.89\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:795.71%' or (concept_cd like '%DX_ID:%' and concept_path like '%\795.71\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:277%' or (concept_cd like '%DX_ID:%' and concept_path like '%\277.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:307.5%' or (concept_cd like '%DX_ID:%' and concept_path like '%\307.5_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:756.5%' or (concept_cd like '%DX_ID:%' and concept_path like '%\756.5_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:783%' or (concept_cd like '%DX_ID:%' and concept_path like '%\783.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:V08%' or (concept_cd like '%DX_ID:%' and concept_path like '%\V08.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:V69.1%' or (concept_cd like '%DX_ID:%' and concept_path like '%\V69.1_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:758%' or (concept_cd like '%DX_ID:%' and concept_path like '%\758.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:191.1%' or (concept_cd like '%DX_ID:%' and concept_path like '%\191.1_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:244.9%' or (concept_cd like '%DX_ID:%' and concept_path like '%\244.9_\%') union all
+-- select * from &dataschema..concept_dimension where concept_cd like 'ICD9:250.01%' or (concept_cd like '%DX_ID:%' and concept_path like '%\250.01%') union all
+-- select * from &dataschema..concept_dimension where concept_cd like 'ICD9:250.03%' or (concept_cd like '%DX_ID:%' and concept_path like '%\250.03%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:253.2%' or (concept_cd like '%DX_ID:%' and concept_path like '%\253.2_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:253.3%' or (concept_cd like '%DX_ID:%' and concept_path like '%\253.3_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:255%' or (concept_cd like '%DX_ID:%' and concept_path like '%\255.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:255.41%' or (concept_cd like '%DX_ID:%' and concept_path like '%\255.41\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:259.1%' or (concept_cd like '%DX_ID:%' and concept_path like '%\259.1_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:259.8%' or (concept_cd like '%DX_ID:%' and concept_path like '%\259.8_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:277.89%' or (concept_cd like '%DX_ID:%' and concept_path like '%\277.89\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:428%' or (concept_cd like '%DX_ID:%' and concept_path like '%\428.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:530.13%' or (concept_cd like '%DX_ID:%' and concept_path like '%\530.13\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:555.9%' or (concept_cd like '%DX_ID:%' and concept_path like '%\555.9_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:556.9%' or (concept_cd like '%DX_ID:%' and concept_path like '%\556.9_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:581.9%' or (concept_cd like '%DX_ID:%' and concept_path like '%\581.9_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:585.6%' or (concept_cd like '%DX_ID:%' and concept_path like '%\585.6_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:756.59%' or (concept_cd like '%DX_ID:%' and concept_path like '%\756.59\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:758.6%' or (concept_cd like '%DX_ID:%' and concept_path like '%\758.6_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:759.81%' or (concept_cd like '%DX_ID:%' and concept_path like '%\759.81\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:759.89%' or (concept_cd like '%DX_ID:%' and concept_path like '%\759.89\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:782.3%' or (concept_cd like '%DX_ID:%' and concept_path like '%\782.3_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:191%' or (concept_cd like '%DX_ID:%' and concept_path like '%\191.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:201%' or (concept_cd like '%DX_ID:%' and concept_path like '%\201.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:202%' or (concept_cd like '%DX_ID:%' and concept_path like '%\202.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:203%' or (concept_cd like '%DX_ID:%' and concept_path like '%\203.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:204%' or (concept_cd like '%DX_ID:%' and concept_path like '%\204.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:205%' or (concept_cd like '%DX_ID:%' and concept_path like '%\205.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:206%' or (concept_cd like '%DX_ID:%' and concept_path like '%\206.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:207%' or (concept_cd like '%DX_ID:%' and concept_path like '%\207.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:208%' or (concept_cd like '%DX_ID:%' and concept_path like '%\208.__\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:714.3%' or (concept_cd like '%DX_ID:%' and concept_path like '%\714.3_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:996.8%' or (concept_cd like '%DX_ID:%' and concept_path like '%\996.8_\%') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:V42.0%' or (concept_cd like '%DX_ID:%' and concept_path like '%\V42.0%\') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:V42.1%' or (concept_cd like '%DX_ID:%' and concept_path like '%\V42.1%\') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:V42.7%' or (concept_cd like '%DX_ID:%' and concept_path like '%\V42.7%\') union all
+select * from &dataschema..concept_dimension where concept_cd like 'ICD9:V42.81%' or (concept_cd like '%DX_ID:%' and concept_path like '%\V42.81\%')
+;
+
+alter table concepts_obesity add (
+  icd9 varchar(50) );
+  
+update concepts_obesity set icd9 = concept_cd where concept_cd like 'ICD9:%';
+update concepts_obesity 
+set icd9 = 'ICD9:'||regexp_substr(concept_path,'E{0,1}[V0-9]{3}\.[0-9]{1,2}')
+where concept_cd like '%DX_ID:%';
+/***** end obesity exclusion diagnoses ****/
+
 -- create table of patients and how many distinct START_DATEs each
 -- had during each year when they had visits
--- SAME FOR ALL STUDIES
 drop table patient_yr_visits;
 create table patient_yr_visits as
 select patient_num, start_date, concept_cd, extract(year from start_date) yr
@@ -48,7 +125,6 @@ from &dataschema..observation_fact
 group by patient_num, start_date, concept_cd;
 
 -- create a table of unique concept descriptors (for filtering later on)
--- SAME FOR ALL STUDIES
 drop table unique_conc;
 create table unique_conc as
 select concept_cd,min(concept_path) concept_path,min(name_char) name_char
@@ -56,7 +132,6 @@ from &dataschema..concept_dimension
 group by concept_cd;
 
 -- merge the two above
--- SAME FOR ALL STUDIES
 drop table patient_yr_conc;
 create table patient_yr_conc as
 select py.*, concept_path, name_char
